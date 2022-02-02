61c61510ccc5f9c590ba6c7462120672f346e994
HGBA <19857811731@163.com>
2022-02-02 15:58:05 +0800

    添加基础表格

--- LWR/frontend/examinationsystem/src/components/Table.vue
+++ LWR/frontend/examinationsystem/src/components/Table.vue
@@ -1,24 +1,14 @@
 <template>
   <table class="table" cellspacing="0">
     <tr>
-      <th>标题1</th>
-      <th>标题2</th>
-      <th>标题3</th>
+      <th v-for="column in columns" :key="column.name">
+        {{ column.title }}
+      </th>
     </tr>
-    <tr>
-      <td>a</td>
-      <td>b</td>
-      <td>c</td>
-    </tr>
-    <tr>
-      <td>a</td>
-      <td>b</td>
-      <td>c</td>
-    </tr>
-    <tr>
-      <td>a</td>
-      <td>b</td>
-      <td>c</td>
+    <tr v-for="(item, index) in items" :key="index">
+      <td v-for="column in columns" :key="column.name">
+        {{ item[column.name] }}
+      </td>
     </tr>
   </table>
   <table class="footer">
@@ -27,9 +17,25 @@
       <td style="text-align: center; font-size: 0">
         <table class="page">
           <tr>
-            <td><svg-icon iconName="left" className="arrow"></svg-icon></td>
-            <td><span class="text">1</span></td>
-            <td><svg-icon iconName="right" className="arrow"></svg-icon></td>
+            <td
+              class="active"
+              :class="{ disable: currentPage <= 1 }"
+              @click="gotoPage(currentPage - 1)"
+            >
+              <svg-icon iconName="left" className="arrow"></svg-icon>
+            </td>
+            <td>
+              <span class="text">{{ currentPage }}</span>
+            </td>
+            <td
+              class="active"
+              :class="{
+                disable: currentPage >= getTotalPages(),
+              }"
+              @click="gotoPage(currentPage + 1)"
+            >
+              <svg-icon iconName="right" className="arrow"></svg-icon>
+            </td>
           </tr>
         </table>
       </td>
@@ -45,8 +51,10 @@ export default {
     return {
       items: [],
       totalCount: 0,
-      order: "",
+      order: null,
       desc: false,
+      itemsPerPage: 3,
+      currentPage: 1,
     };
   },
   methods: {
@@ -54,18 +62,39 @@ export default {
       axios({
         url: this.url,
         data: {
-          offset: this.itemsPerPage * this.currentPage,
+          offset: this.itemsPerPage * (this.currentPage - 1),
           max: this.itemsPerPage,
           order: this.order,
           desc: this.desc,
         },
       })
         .then((response) => {
-          console.log(response.data);
+          this.totalCount = response.data.count;
+          this.items = response.data.data;
         })
         .catch(() => {
           alert("请求数据失败！");
         });
+    },
+    getTotalPages() {
+      let totalPages = Math.floor(this.totalCount / this.itemsPerPage);
+      if (this.totalCount % this.itemsPerPage != 0) totalPages++;
+      return totalPages;
+    },
+    gotoPage(page) {
+      let newPage = this.currentPage;
+
+      if (page <= 0) newPage = 1;
+      else {
+        let totalPages = this.getTotalPages();
+        if (page > totalPages) newPage = totalPages;
+        else newPage = page;
+      }
+
+      if (newPage != this.currentPage) {
+        this.currentPage = newPage;
+        this.getItems();
+      }
     },
   },
   watch: {
@@ -79,9 +108,7 @@ export default {
   props: {
     url: { type: String, default: "" },
     //对象格式{title:"title",name:"name",transformer:(data)=>data}
-    column: { type: Array, default: () => [] },
-    itemsPerPage: { type: Number, default: 10 },
-    currentPage: { type: Number, default: 1 },
+    columns: { type: Array, default: () => [] },
   },
 };
 </script>
@@ -136,5 +163,14 @@ export default {
 .page .arrow {
   width: 18px;
   height: 18px;
+}
+
+.page .active {
+  cursor: pointer;
+}
+
+.page .disable {
+  opacity: 0.2;
+  cursor: default;
 }
 </style>
--- LWR/frontend/examinationsystem/src/views/QuestionsManage.vue
+++ LWR/frontend/examinationsystem/src/views/QuestionsManage.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>题库管理</h1>
+  <div>
+    <Card>
+      <template v-slot:headerLeft> 题库列表 </template>
+      <template v-slot:headerRight>
+        <table>
+          <tr>
+            <td style="font-size: 0"><SearchBox></SearchBox></td>
+            <td>
+              <button class="btn" style="margin-left: 30px">搜索题库</button>
+            </td>
+          </tr>
+        </table>
+      </template>
+      <template v-slot:content>
+        <div class="content">
+          <Table url="test/test" :columns="columns"></Table>
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
+
+<script>
+import Card from "@/components/Card.vue";
+import SearchBox from "@/components/SearchBox.vue";
+import Table from "@/components/Table.vue";
+export default {
+  data() {
+    return {
+      columns: [
+        { title: "id", name: "id" },
+        { title: "标题1", name: "a" },
+        { title: "标题2", name: "b" },
+        { title: "标题3", name: "c" },
+      ],
+    };
+  },
+  components: {
+    Card,
+    SearchBox,
+    Table,
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  padding: 20.5px;
+}
+</style>
