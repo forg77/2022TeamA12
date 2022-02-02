bd80e5d67a496b35eb03420b6636e959f5a3795b
HGBA <19857811731@163.com>
2022-02-02 13:00:26 +0800

    配置axios

--- LWR/frontend/examinationsystem/src/config.js
+++ LWR/frontend/examinationsystem/src/config.js
@@ -1,4 +1,10 @@
+import axios from "axios";
+
 const siteTitle = "考试系统";
-const serverUrl = "http://139.9.58.231:8080/";
+// const serverUrl = "http://139.9.58.231:8080/";
+const serverUrl = "http://localhost:8082/";
+
+axios.defaults.baseURL = serverUrl;
+axios.defaults.method = 'post';
 
 export { siteTitle, serverUrl };
