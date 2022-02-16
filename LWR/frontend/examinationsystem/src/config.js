709b6991c16f9204480b6bf848675474b7f290a9
HGBA <19857811731@163.com>
2022-02-16 19:26:42 +0800

    配置登录拦截器

--- LWR/frontend/examinationsystem/src/config.js
+++ LWR/frontend/examinationsystem/src/config.js
@@ -1,10 +1,32 @@
 import axios from "axios";
+import router from './router'
 
+//网站的标题
 const siteTitle = "考试系统";
-const serverUrl = "http://139.9.58.231:8080/";
-// const serverUrl = "http://localhost:8082/";
+//后台服务器地址
+// let serverUrl = "http://139.9.58.231:8080/";
+const serverUrl = "http://localhost:8082/";
+//后台登录地址
+const loginUrl = "user/login";
 
+//配置axios默认值
 axios.defaults.baseURL = serverUrl;
 axios.defaults.method = 'post';
+axios.defaults.withCredentials = true;
 
-export { siteTitle, serverUrl };
+//配置axios拦截器
+axios.interceptors.response.use((response) => {
+    //配置用户未登录的处理
+    if (response.config.url != "/user/userInfo") {
+        let errCode = response.data && response.data.errCode;
+        if (errCode == 101) {
+            router.replace("/login");
+        }
+    }
+    return response;
+});
+
+//当前登录的用户信息
+const user = null;
+
+export default { siteTitle: siteTitle, serverUrl: serverUrl, user: user, loginUrl: loginUrl };
