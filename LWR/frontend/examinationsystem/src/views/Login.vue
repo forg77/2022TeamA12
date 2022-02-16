f9061731a8d2fd08329c6cd08a68c9fba45d7fd9
HGBA <19857811731@163.com>
2022-02-16 19:25:59 +0800

    添加登录页面

--- LWR/frontend/examinationsystem/src/views/Login.vue
+++ LWR/frontend/examinationsystem/src/views/Login.vue
@@ -1,3 +1,38 @@
 <template>
-  <button class="btn">点击登录</button>
+  <button class="btn" @click="login('admin1', '123')">点击登录</button>
 </template>
+
+<script>
+import axios from "axios";
+import config from "../config";
+export default {
+  data() {
+    return {
+      ajaxCancel: null,
+    };
+  },
+  methods: {
+    login(username, password) {
+      if (this.ajaxCancel != null) {
+        this.ajaxCancel();
+        this.ajaxCancel = null;
+      }
+      axios({
+        url: config.loginUrl,
+        cancelToken: new axios.CancelToken((c) => {
+          this.ajaxCancel = c;
+        }),
+        data: {
+          username: username,
+          password: password,
+        },
+      }).then((response) => {
+        if (response.data.errCode == 0) {
+          config.user = response.data.userData;
+          this.$router.replace("/admin");
+        }
+      });
+    },
+  },
+};
+</script>
