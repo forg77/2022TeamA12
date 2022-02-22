cba51f4c6ba17af43d4e96fbbc44b2b775161d30
HGBA <19857811731@163.com>
2022-02-22 11:49:15 +0800

    添加插入错误

--- LSH/ExaminationSystem/src/main/java/edu/zstu/examsys/pojo/ErrorCode.java
+++ LSH/ExaminationSystem/src/main/java/edu/zstu/examsys/pojo/ErrorCode.java
@@ -7,4 +7,6 @@ public class ErrorCode {
     public static final int SUCCESS = 0;
     public static final int NO_LOGIN = 101;
     public static final int LOGIN_FAILED = 102;
+
+    public static final int INSERT_FAILED = 201;
 }
