e5358867e0ae391eb7c376ba771eebfa069ab278
yourusername <youremail>
2022-02-23 11:36:02 +0800

    小卡片完成

--- LWR/frontend/examinationsystem/src/components/ExamCard.vue
+++ LWR/frontend/examinationsystem/src/components/ExamCard.vue
@@ -2,36 +2,53 @@
   <div class="card-container">
     <div class="head">
       <div class="exam-name">马克思主义基本原理概论</div>
-      <div class="button"><span class="btncontent">管理</span></div>
+      <div class="button"><span class="btncontent">考试</span></div>
     </div>
 
     <div class="content">马克思主义基本原理概论-2019年上期末考试</div>
 
-    <div class="foot">
-      <div class="time">2020-01-23</div>
-      <div class="limit-time">限时120分钟</div>
-      <div class="score">满分100</div>
-    </div>
+    <table style="width: 428px" class="footer">
+      <tbody>
+        <td>
+          <div class="time">
+            <svg-icon iconName="calendar" className="calendar-icon"></svg-icon
+            >&nbsp;2020-01-23
+          </div>
+        </td>
+        <td>
+          <div class="limit-time">
+            <svg-icon iconName="time" className="time-icon"></svg-icon
+            >&nbsp;限时120分钟
+          </div>
+        </td>
+        <td>
+          <div class="score">
+            <svg-icon iconName="100" className="100-icon"></svg-icon
+            >&nbsp;满分100
+          </div>
+        </td>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <style scoped>
 .head {
   display: flex;
-  border: solid;
   height: auto;
   align-items: center;
   justify-content: space-between;
 }
 .card-container {
   width: 470px;
-  height: 220px;
+  height: 250px;
   background: #ffffff;
   border: 1px solid rgba(0, 0, 0, 0.10196078431372549);
   box-shadow: 2px -3px 6px rgba(0, 0, 0, 0.1);
   opacity: 1;
   border-radius: 10px;
   padding: 40px 20px 0 20px;
+  position: relative;
 }
 .exam-name {
   width: 264px;
@@ -69,14 +86,15 @@
   font-weight: 400;
   color: #000000;
   opacity: 0.7;
-  border: solid;
+  margin-top: 20px;
 }
-.foot {
-  display: flex;
-  justify-content: space-around;
+.footer {
+  position: absolute;
+  bottom: 20px;
 }
-.time,.limit-time,.score {
-  border: solid;
+.time,
+.limit-time,
+.score {
   width: 89px;
   height: 21px;
   font-size: 12px;
@@ -84,5 +102,6 @@
   font-weight: 400;
   color: #000000;
   opacity: 0.7;
+  margin: auto;
 }
 </style>
