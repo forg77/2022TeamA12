93756bebb449f10e0995d0bdf143f2e61aa8b2cb
HGBA <19857811731@163.com>
2022-03-08 13:56:13 +0800

    ts测试

--- LWR/frontend/examinationsystem/src/views/About.vue
+++ LWR/frontend/examinationsystem/src/views/About.vue
@@ -3,3 +3,25 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+interface Book {
+  title: string;
+  author: string;
+  year: number;
+}
+
+export default defineComponent({
+  data() {
+    return {
+      book: {
+        title: 'Vue 3 Guide',
+        author: 'Vue Team',
+        year: 2020,
+      }
+    }
+  }
+})
+</script>
