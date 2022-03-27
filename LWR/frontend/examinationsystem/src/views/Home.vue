7825c43f11f77dc137329b3c06bc6014ee8588d1
yourusername <youremail>
2022-03-27 09:46:02 +0800

    上传

--- LWR/frontend/examinationsystem/src/views/Home.vue
+++ LWR/frontend/examinationsystem/src/views/Home.vue
@@ -1,278 +1,341 @@
 <template>
-  <div class="container">
-    <div class="head">
-      <el-steps :space="200" :active="1" simple>
-        <el-step title="创建方式" :icon="EditPen" />
-        <el-step title="试卷设置" :icon="Setting" />
-        <el-step title="添加考生" :icon="User" />
-      </el-steps>
-    </div>
-    <div class="content1" v-if="false">
-      <div>
-        选择创建方式
-        <el-icon color="#338AFB" :size="30" style="float: right"
-          ><Back
-        /></el-icon>
-      </div>
-      <el-divider />
-      <div style="margin: auto; width: 345px">
-        <el-radio v-model="radio1" label="1" size="large"
-          >手动组卷（手动编辑创建新新试卷）</el-radio
-        >
-        <br />
-        <el-radio v-model="radio1" label="2" size="large"
-          >自动随机组卷（系统从已有题库中随机选择题组卷）</el-radio
-        >
-      </div>
-      <br />
-      <div style="width: 190px; margin: auto">
-        <el-button type="primary">立即创建</el-button>
-        <el-button>取消</el-button>
-      </div>
-    </div>
+  <div class="outer" v-if="true">
+    <div style="margin-left: auto; margin-right: 30px">
+      <Card :width="650" :headerHeight="64">
+        <template v-slot:headerLeft>
+          成绩分析
+          <span style="font-size: 15px; color: #9aa2b5">平均分：</span>
+          <span style="font-size: 15px; color: #4089ff">10 </span>
+          <span style="font-size: 15px; color: #9aa2b5">方差：</span>
+          <span style="font-size: 15px; color: #4089ff">0.8</span>
+        </template>
 
-    <div class="content2" v-if="true">
+        <template v-slot:headerRight>
+          <button class="BTN">成绩分布</button><button class="BTN">全部</button>
+        </template>
+
+        <template v-slot:content>
+          <div ref="grade" style="width: 100%; height: 400px"></div>
+        </template>
+      </Card>
       <div>
-        试卷设置
-        <el-icon color="#338AFB" :size="30" style="float: right"
-          ><Back
-        /></el-icon>
-      </div>
-      <el-divider />
-      <div>
-        <el-form :model="form1" label-width="120px">
-          <el-form-item label="标题：">
-            <el-input
-              v-model="form1.title"
-              clearable
-              style="width: 400px"
-              placeholder="请输入试卷的标题"
-            />
-          </el-form-item>
-          <el-form-item label="学年：">
-            <el-select v-model="form1.year" placeholder="选择学年">
-              <el-option label="2019" value="2019" />
-              <el-option label="2020" value="2020" />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="平台：">
-            <el-checkbox-group v-model="form1.checklist">
-              <el-checkbox label="移动端" />
-              <el-checkbox label="WEB端" />
-            </el-checkbox-group>
-          </el-form-item>
-          <el-form-item label="难度：">
-            <el-select v-model="form1.difficulty" placeholder="选择难度">
-              <el-option label="简单" value="easy" />
-              <el-option label="中等" value="normal" />
-              <el-option label="困难" value="difficult" />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="时间：">
-            <el-date-picker
-              v-model="form1.time"
-              type="datetimerange"
-              start-placeholder="开始时间"
-              end-placeholder="结束时间"
-            />
-            <span>
-              <el-icon color="#338AFB" :size="20"><Timer /></el-icon>
-              共计：100分钟 30秒</span
+        <Card :width="650" :headerHeight="64">
+          <template v-slot:headerLeft> 教学建议 </template>
+          <template v-slot:content>
+            <table
+              style="
+                width: 80%;
+                margin-left: 30px;
+                border-collapse: separate;
+                border-spacing: 0px 20px;
+              "
             >
-          </el-form-item>
-          <el-form-item label="总分：">
-            <el-input
-              v-model="form1.totalscore"
-              clearable
-              style="width: 200px"
-              placeholder="请输入试卷的总分"
-            />
-          </el-form-item>
-          <el-form-item label="作弊：">
-            <el-switch v-model="form1.switch" /><span>&nbsp;开启反作弊</span>
-          </el-form-item>
-        </el-form>
-      </div>
-
-      <div style="margin-left: 60px; margin-right: 60px">
-        <el-card shadow="never">
-          <template #header>
-            <div>
-              <span>单选题 </span>
-              <el-icon><Delete /></el-icon>
-            </div>
+              <tr>
+                <td><div class="line" style="background-color: blue"></div></td>
+                <td>
+                  总体平均分：<span
+                    >总体平均分与难度系数相匹配，主要集中在70分左右。</span
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td><div class="line" style="background-color: pink"></div></td>
+                <td>方差：<span>方差大，适当提供复习/习题课</span></td>
+              </tr>
+              <tr>
+                <td><div class="line" style="background-color: red"></div></td>
+                <td>
+                  知识点平均分：<span
+                    >部分题目正确率较低，适当加强XXX知识点的讲解</span
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div class="line" style="background-color: green"></div>
+                </td>
+                <td>总结：<span>造化钟神秀</span></td>
+              </tr>
+            </table>
           </template>
-          <div>
-            <el-form :model="choice">
-              <el-form-item label="总分：">
-                <el-input clearable style="width: 200px" v-model="choice.total" />
-              </el-form-item>
-              <el-form-item label="题型说明：">
-                <el-input
-                  clearable
-                  style="width: 172px"
-                  v-model="choice.describe"
-                />
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="choice.select" label="1" size="large"
-                  >从题库选题
-                  <span>
-                    共 999+ 道 抽
-                    <input
-                      type="number"
-                      style="
-                        border-radius: 5px;
-                        width: 30px;
-                        border: solid rgb(220, 223, 230) 0.7px;
-                        outline: none;
-                        height: 25px;
-                      "
-                      v-model="choice.number"
-                    />
-                    道
-                  </span>
-                </el-radio>
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="choice.select" label="2" size="large"
-                  >从题库指定目录选题</el-radio
-                >
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="choice.select" label="3" size="large"
-                  >从题库按照难以度选题</el-radio
-                >
-              </el-form-item>
-            </el-form>
-          </div>
-        </el-card>
-        <br />
-        <el-card shadow="never">
-          <template #header>
-            <div>
-              <span>填空题 </span>
-              <el-icon><Delete /></el-icon>
-            </div>
-          </template>
-          <div>
-            <el-form :model="fill">
-              <el-form-item label="总分：">
-                <el-input clearable style="width: 200px" v-model="fill.total" />
-              </el-form-item>
-              <el-form-item label="题型说明：">
-                <el-input
-                  clearable
-                  style="width: 172px"
-                  v-model="fill.describe"
-                />
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="fill.select" label="1" size="large"
-                  >从题库选题
-                  <span>
-                    共 999+ 道 抽
-                    <input
-                      type="number"
-                      style="
-                        border-radius: 5px;
-                        width: 30px;
-                        border: solid rgb(220, 223, 230) 0.7px;
-                        outline: none;
-                        height: 25px;
-                      "
-                      v-model="fill.number"
-                    />
-                    道
-                  </span>
-                </el-radio>
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="fill.select" label="2" size="large"
-                  >从题库指定目录选题</el-radio
-                >
-              </el-form-item>
-              <el-form-item>
-                <el-radio v-model="fill.select" label="3" size="large"
-                  >从题库按照难以度选题</el-radio
-                >
-              </el-form-item>
-            </el-form>
-          </div>
-        </el-card>
-      </div>
-
-      <div style="width: 190px; margin: auto">
-        <el-button type="primary">立即创建</el-button>
-        <el-button>取消</el-button>
+        </Card>
       </div>
     </div>
-
-    <div class="content3" v-if="false">
+    <div>
+      <Card :width="750" style="margin-right: auto" :headerHeight="64">
+        <template v-slot:headerLeft> 知识点 </template>
+        <template v-slot:headerRight>
+          <button class="BTN" style="width: 200px">
+            科目:马克思主义基本原理概论</button
+          ><button class="BTN">分类:领域</button>
+        </template>
+        <template v-slot:content>
+          <div ref="point1" style="width: 32%; height: 200px"></div>
+          <div ref="point2" style="width: 32%; height: 200px"></div>
+        </template>
+      </Card>
       <div>
-        添加考生
-        <el-icon color="#338AFB" :size="30" style="float: right"
-          ><Back
-        /></el-icon>
+        <Card :width="750" style="margin-right: auto" :headerHeight="64">
+          <template v-slot:headerLeft> 成绩详情 </template>
+          <template v-slot:content>
+            <div style="display: flex">
+              <div>
+                <span>班级/年级平均分</span>
+                <br />
+                <span>77.5</span>
+                <br />
+                <span>总分</span>
+              </div>
+              <div ref="Gauge" style="width: 40%; height: 250px"></div>
+              <div>
+                <div> 试卷难度系数： </div>
+                <div> 班级/年级最高： </div>
+                <div> 班级/年级最高： </div>
+                <div> 班级/年级方差： </div>
+              </div>
+            </div>
+          </template>
+        </Card>
       </div>
-      <el-divider />
     </div>
+  </div>
+  <div class="dialog" v-if="false">
+    <Card :width="750" style="margin-right: auto" :headerHeight="64">
+      <template v-slot:headerLeft> 考试列表 </template>
+      <template v-slot:headerRight>
+        <Dropdown></Dropdown>
+        <Search></Search>
+        <button class="search">搜索考试</button>
+      </template>
+      <template v-slot:content>
+        <table
+          style="border-collapse: separate; border-spacing: 40px 10px"
+          frame="below"
+        >
+          <tr>
+            <th>序号</th>
+            <th>考试名称</th>
+            <th>考试状态</th>
+            <th>参与人数</th>
+            <th>考试时间</th>
+          </tr>
+          <tr>
+            <td><input type="checkbox" />1</td>
+            <td>马克思主义基本原理概论</td>
+            <td>已结束</td>
+            <td>800</td>
+            <td>2020-01-25</td>
+          </tr>
+        </table>
+      </template>
+    </Card>
   </div>
 </template>
 
+<script lang="ts">
+import Card from "@/components/Card.vue";
+import Search from "@/components/SearchBox.vue";
+import Dropdown from "@/components/DropDown.vue";
+import { defineComponent } from "vue";
+import AccountManage from "@/views/Personal/AccountManage.vue";
+import BasicInfo from "@/views/Personal/BasicInfo.vue";
+import * as echarts from "echarts";
 
-<script lang="ts" setup>
-import {
-  EditPen,
-  Setting,
-  User,
-  Back,
-  Timer,
-  Delete,
-} from "@element-plus/icons-vue";
-import { ref } from "vue";
-import { reactive } from "vue";
+export default defineComponent({
+  components: { Card, BasicInfo, AccountManage, Search, Dropdown },
+  data() {
+    return {
+      content: "AccountManage",
+    };
+  },
+  mounted() {
+    const g = this.$refs.grade as HTMLDivElement;
+    const p1 = this.$refs.point1 as HTMLDivElement;
+    const G = this.$refs.Gauge as HTMLDivElement;
+    const p2 = this.$refs.point2 as HTMLDivElement;
 
-const radio1 = ref("1");
-const form1 = reactive({
-  title: "",
-  year: "",
-  checklist: [],
-  difficulty: "",
-  time: "",
-  totalscore: "",
-  switch: false,
-});
-const fill = reactive({
-  total: "",
-  describe: "",
-  select: "",
-  number: "",
-});
+    // 基于准备好的dom，初始化echarts实例
+    var gradeChart = echarts.init(g);
+    var pointChart = echarts.init(p1);
+    var GaugeChart = echarts.init(G);
 
-const choice = reactive({
-  total: "",
-  describe: "",
-  select: "",
-  number: "",
+    // 绘制图表
+    pointChart.setOption({
+      tooltip: {
+        trigger: "item",
+      },
+      legend: {
+        top: "5%",
+        left: "center",
+      },
+      series: [
+        {
+          name: "Access From",
+          type: "pie",
+          radius: ["40%", "70%"],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: "center",
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: "40",
+              fontWeight: "bold",
+            },
+          },
+          labelLine: {
+            show: false,
+          },
+          data: [
+            { value: 1048, name: "Search Engine" },
+            { value: 735, name: "Direct" },
+            { value: 580, name: "Email" },
+            { value: 484, name: "Union Ads" },
+            { value: 300, name: "Video Ads" },
+          ],
+        },
+      ],
+    });
+    gradeChart.setOption({
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: ["0-20", "20-40", "40-50", "50-60", "60-70", "70-80", "80-100"],
+          axisTick: {
+            alignWithLabel: true,
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+        },
+      ],
+      series: [
+        {
+          name: "Direct",
+          type: "bar",
+          barWidth: "60%",
+          data: [10, 52, 200, 334, 390, 330, 220],
+        },
+      ],
+    });
+    GaugeChart.setOption({
+      series: [
+        {
+          type: "gauge",
+          progress: {
+            show: true,
+            width: 18,
+          },
+          axisLine: {
+            lineStyle: {
+              width: 18,
+            },
+          },
+          axisTick: {
+            show: false,
+          },
+          splitLine: {
+            length: 15,
+            lineStyle: {
+              width: 2,
+              color: "#999",
+            },
+          },
+          axisLabel: {
+            distance: 25,
+            color: "#999",
+            fontSize: 10,
+          },
+          anchor: {
+            show: true,
+            showAbove: true,
+            size: 10,
+            itemStyle: {
+              borderWidth: 10,
+            },
+          },
+          title: {
+            show: false,
+          },
+          detail: {
+            valueAnimation: true,
+            fontSize: 10,
+            offsetCenter: [0, "70%"],
+          },
+          data: [
+            {
+              value: 70,
+            },
+          ],
+        },
+      ],
+    });
+  },
 });
 </script>
-
-<style>
-.container {
-  height: auto;
-  width: 800px;
-  margin: auto;
+<style lang="scss" scoped>
+.outer {
+  display: flex;
+}
+.switch {
+  width: 68px;
+  height: 30px;
+  background: #409eff;
+  opacity: 1;
+  border-radius: 4px;
+  color: white;
+  border: none;
+  outline: none;
+}
+.BTN {
+  width: 80px;
+  height: 32px;
+  border: 0.5px solid #4089ff;
+  opacity: 1;
+  border-radius: 0px;
   background-color: #fff;
+  color: #9aa2b5;
+  outline: none;
 }
-.head {
-  width: 100%;
-  height: auto;
+.BTN:hover {
+  background-color: #e1edf6;
 }
-.content1,
-.content2,
-.content3,
-.content4 {
-  padding: 20px;
+.line {
+  height: 20px;
+  width: 8px;
+}
+.dialog {
+  width: 800px;
+  height: 683px;
+  background: #ffffff;
+  border: 1px solid #707070;
+  opacity: 1;
+  border-radius: 0px;
+}
+.search {
+  width: 120px;
+  height: 40px;
+  background: #338afb;
+  opacity: 1;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  outline: none;
 }
 </style>
