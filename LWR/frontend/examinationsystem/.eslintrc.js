b9e16c72ad9aa80bda71857dfd7524389dcb3b5a
HGBA <19857811731@163.com>
2022-03-28 11:46:26 +0800

    增加eslint配置

---
+++ LWR/frontend/examinationsystem/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "vue/no-mutating-props":"off",
+        "vue/multi-word-component-names":"off",
+        "vue/no-unused-components":"off",
+        "no-unused-vars":"off",
+        "vue/no-v-model-argument":"off"
+    }
+};
